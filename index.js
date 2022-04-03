f5f6bb0d23735bc87238614871e90342c1e9edcc
Lucas-Esteban-Rodriguez <eteban2412@gmail.com>
2022-04-02 22:32:04 -0300

    Arreglando simulador interactivo

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+let num1
+let num2
+let operador
+
+
+num1 = parseInt(prompt ('Ingrese el primer numero'))
+operador = prompt ('Ingrese operador')
+num2 = parseInt(prompt ('Ingrese el segundo numero'))
+alert (num1 + operador + num2 + '=' + calculadora(num1,num2,operador))
 function calculadora(num1,num2,operador) {
     switch (operador) {
         case '+':
             return num1+num2;
-            if (operador == '=') {
                 break;  
-            }
+        case '-':
+            return num1-num2;
+                break;  
+        case '*':
+            return num1*num2;
+                break;  
+        case '/':
+            return num1/num2;
+                break;  
+        case '=':
+            return num1+num2;
+                break;  
     }
 }
