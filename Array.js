a15c4e666e1015c4de6205e3c1722262931d9015
Lucas-Esteban-Rodriguez <eteban2412@gmail.com>
2022-04-09 15:51:56 -0300

    Iniciando primer entrega de proyecto final

--- Array.js
+++ Array.js
@@ -2,10 +2,15 @@ const array = []
 let nombre
 let dato
 let cierre
+let eliminar
+let datoeliminado
+let i = 1
+let index = 1
 do {
     nombre = prompt('Nombre del dato que quieres agregar').toLowerCase()
     dato = prompt('Ingrese el valor del dato')
-    array.push(' '+nombre +': '+ dato)
+    array.push(' ' +i+ '-' +nombre +': '+ dato)
     cierre = confirm('¿quiere continuar la carga?')
+    i++
 } while (cierre != false);
 alert('Tus datos son: '+array)
