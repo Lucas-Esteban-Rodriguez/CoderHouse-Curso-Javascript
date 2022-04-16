058608f7e6f09d3dc2b39c69b13b1df88107fb2d
Lucas-Esteban-Rodriguez <eteban2412@gmail.com>
2022-04-16 13:26:29 -0300

    Arreglos primera entrega de proyecto final

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let pn
 let operador
 let sn
+let historial = []
 
+// Funcion Resolver
 function resolver(pn,sn,operador) {
     switch (operador) {
         case '+':
@@ -19,19 +21,38 @@ function resolver(pn,sn,operador) {
     }
 }
 
+// Entrada de datos, confirmación para agregar más y salida
+
+pn = parseFloat(prompt('Ingrese el primer número'))
+operador = prompt('Ingrese el operador')
+sn = parseFloat(prompt('Ingrese el segundo número'))
+resultado = resolver(pn,sn,operador)
+historial.push(resultado)
+alert(resultado)
 do {
-    pn = parseFloat(prompt('Ingrese el primer número'))
-    operador = prompt('Ingrese el operador')
-    sn = parseFloat(prompt('Ingrese el segundo número'))
-    resultado = resolver(pn,sn,operador)
-    alert(resultado)
     seguir = confirm('¿Quiere seguir agregando operaciones?')
     if (seguir != false) {
         operador = prompt('Ingrese el operador')
         sn = parseFloat(prompt('Ingrese el siguiente número'))
         resultado = resolver(resultado,sn,operador)
+        historial.push(resultado)
+        alert(resultado)
     } else {
         break
     }
-    alert(resultado)
-} while (false);
+} while (seguir != false);
+alert(resultado)
+
+// Pasar datos del historial al HTML
+
+const ul = document.getElementById('historial')
+for (const dato of historial) {
+    const li = document.createElement('li')
+    li.innerHTML = dato
+    ul.appendChild(li)
+}
+
+// Evento: Abrir historial al clickear boton
+const contenedor__historial = document.getElementById('contenedor__historial')
+const btn__historial = document.getElementById('btn__historial')
+btn__historial.onclick = () => {contenedor__historial.style.display = 'flex'}
