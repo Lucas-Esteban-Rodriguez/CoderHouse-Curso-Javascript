d51f1faa48375e9620eb152d0b4a5112fc24b910
Lucas-Esteban-Rodriguez <eteban2412@gmail.com>
2022-04-25 18:44:45 -0300

    Segunda entrega de proyecto final

--- index.js
+++ index.js
@@ -1,99 +1,62 @@
+// Obtener Historial del LocalStorage
 
-
-// Selecionando los botones que hay en html
-const uno = document.getElementById('uno')
-const dos = document.getElementById('dos')
-const tres = document.getElementById('tres')
-const cuatro = document.getElementById('cuatro')
-const cinco = document.getElementById('cinco')
-const seis = document.getElementById('seis')
-const siete = document.getElementById('siete')
-const ocho = document.getElementById('ocho')
-const nueve = document.getElementById('nueve')
-const cero = document.getElementById('cero')
-const igual = document.getElementById('igual')
-const coma = document.getElementById('coma')
-const remove = document.getElementById('remove')
-const division = document.getElementById('division')
-const multiplicacion = document.getElementById('multiplicacion')
-const suma = document.getElementById('suma')
-const resta = document.getElementById('resta')
-const text = document.getElementById('text')
-
-// Con el evento .onclick logro que se muestre el caracter del boton que estamos apretando
-
-uno.onclick = () => {
-    text.textContent = text.textContent + '1';
-}
-dos.onclick = () => {
-    text.textContent = text.textContent + '2';
-}
-tres.onclick = () => {
-    text.textContent = text.textContent + '3';
-}
-cuatro.onclick = () => {
-    text.textContent = text.textContent + '4';
-}
-cinco.onclick = () => {
-    text.textContent = text.textContent + '5';
-}
-seis.onclick = () => {
-    text.textContent = text.textContent + '6';
-}
-siete.onclick = () => {
-    text.textContent = text.textContent + '7';
-}
-ocho.onclick = () => {
-    text.textContent = text.textContent + '8';
-}
-nueve.onclick = () => {
-    text.textContent = text.textContent + '9';
-}
-cero.onclick = () => {
-    text.textContent = text.textContent + '0';
-}
-division.onclick = () => {
-    text.textContent = text.textContent + '/';
-}
-suma.onclick = () => {
-    text.textContent = text.textContent + '+';
-}
-resta.onclick = () => {
-    text.textContent = text.textContent + '-';
-}
-multiplicacion.onclick = () => {
-    text.textContent = text.textContent + '*';
-}
-coma.onclick = () => {
-    text.textContent = text.textContent + ',';
-}
-igual.onclick = () => {
-}
-remove.onclick = () => {
+for (let index = 0; index < localStorage.length; index++) {
+    let item = getItem(index)
+    EnviaralHistorial(item)
 }
 
+// Funciones de agregar, obtener y borrar del Localstorage
 
-// Declaracion de variables
-let num1
-let operador
-let num2
+function setItem(clave,valor) {
+    localStorage.setItem(clave,valor)
+}
+
+function getItem(clave) {
+    return localStorage.getItem(clave)
+}
+
+function removeItem() {
+    localStorage.clear()
+    location.reload()
+}
+
+// Obteniendo elemento para utilizar en funciones y declarando variable resultado para el historial
+
+const texto_calculadora = document.getElementById ('text')
 let resultado
-let historial = []
 
-// Funcion Resolver
-function resolver(num1,num2,operador) {
-    switch (operador) {
-        case '+':
-            return parseFloat(num1) + parseFloat(num2);
-            break;
-        case '-':
-            return parseFloat(num1) - parseFloat(num2);
-            break;
-        case '*':
-            return parseFloat(num1) * parseFloat(num2);
-            break;
-        case '/':
-            return parseFloat(num1) / parseFloat(num2);
-            break;
+// Armando funciones esenciales: Mostrar elementos en el dom, remover elementos, reiniciar la calculadora y
+// reolver las cuentas
+
+function MostrarDom(e) {
+    texto_calculadora.textContent += e
+}
+
+function Remove() {
+    let text = texto_calculadora.innerHTML
+    texto_calculadora.innerHTML = text.substring(0, text.length -1)
+}
+
+function Reiniciar() {
+    texto_calculadora.textContent = ''
+}
+
+function Resolver() {
+    if (texto_calculadora.innerHTML) {
+        let text = texto_calculadora.innerHTML
+        resultado = eval(text)
+        EnviaralHistorial(texto_calculadora.innerHTML +' = '+ resultado)
+        let index = localStorage.length
+        setItem(index,texto_calculadora.innerHTML +' = '+ resultado)
+        texto_calculadora.textContent = resultado
+    } else {
+        texto_calculadora.textContent = 'error'
     }
+}
+
+function EnviaralHistorial(dato) {
+    const ul = document.getElementById('ul')
+    const li = document.createElement('li')
+    li.innerHTML = dato
+    ul.appendChild(li)
 }
